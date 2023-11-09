738f9a3f35412afe046243ad30a21b9dcf3c986e
moisesmarin21 <marin.mmoises.456@gmail.com>
2023-11-09 03:39:32 -0500

    moises

---
+++ database/seeders/PedidoSeeder.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Carbon\Carbon;
+
+class PedidoSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+
+        $pedido = [
+            ['descripcion' => 'Arroz con pato','precio' => 25.00,'cantidad' => 2,'tipo' => 'Segundo','fecha' => now(),'estado' => 1,'idCliente' => 1],
+            ['descripcion' => 'Pollo frito','precio' => 20.00,'cantidad' => 3,'tipo' => 'Segundo','fecha' => now(),'estado' => 1,'idCliente' => 2],
+            ['descripcion' => 'Cuy frito','precio' => 28.00,'cantidad' => 2,'tipo' => 'Segundo','fecha' => now(),'estado' => 1,'idCliente' => 3],
+            ['descripcion' => 'Arroz con pollo','precio' => 18.00,'cantidad' => 1,'tipo' => 'Segundo','fecha' => now(),'estado' => 2,'idCliente' => 4],
+            ['descripcion' => 'Caldo de gallina','precio' => 7.00,'cantidad' => 4,'tipo' => 'Entrada','fecha' => now(),'estado' => 2,'idCliente' => 5],
+            ['descripcion' => 'Papa huancaina','precio' => 7.00,'cantidad' => 3,'tipo' => 'Entrada','fecha' => now(),'estado' => 1,'idCliente' => 6],
+            ['descripcion' => 'Jarra de Cebada','precio' => 12.00,'cantidad' => 2,'tipo' => 'Refresco','fecha' => now(),'estado' => 2,'idCliente' => 7],
+            ['descripcion' => 'Arroz con pato','precio' => 25.00,'cantidad' => 1,'tipo' => 'Segundo','fecha' => now(),'estado' => 2,'idCliente' => 8],
+            ['descripcion' => 'Ensalada','precio' => 5.00,'cantidad' => 6,'tipo' => 'Entrada','fecha' => now(),'estado' => 2,'idCliente' => 9],
+            ['descripcion' => 'Arroz con chancho','precio' => 26.00,'cantidad' => 2,'tipo' => 'Segundo','fecha' => now(),'estado' => 1,'idCliente' => 10],
+        ];
+
+        // Insertar los datos en la tabla "mesas"
+        DB::table('pedidos')->insert($pedido);
+
+    } 
+    
+}
--- resources/views/pedidos/personalPedidos/consulta/graficos.blade.php
+++ resources/views/pedidos/personalPedidos/consulta/graficos.blade.php
@@ -4,29 +4,129 @@
 
 @section('mainContent')
     <!DOCTYPE html>
-    <html lang="en">
+    <html lang="es">
     <head>
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title>Gráfica con Chart.js</title>
+        <title>Gráfica Estadística</title>
         <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
     </head>
-    <body>
+    
+    <body">
+
         <div align="center">
             <div class="row">
-                <div class="col-6">
-                    <canvas id="barras" width="20" height="10"></canvas>
+                <div style="width:50%;" class="col-6">
+                    <canvas id="barras"></canvas>
                 </div>
-                <div class="col-6">
-                    <canvas id="lineas" width="20" height="10"></canvas>
+                <div style="width:50%;" class="col-6">
+                    <canvas id="lineas"></canvas>
                 </div>
             </div>
-            <br><br>
-            <div class="col-6">
-                <canvas id="circular" width="20" height="10"></canvas>
+            <br>
+            <div style="width:32%;" class="col-12">
+                <canvas id="circular"></canvas>
             </div>
         </div>
-        <script src="../grafica.js"></script>
+
+        <!----------------------------GRAFICO DE BARRAS------------------------------->
+        <script>
+            var ctx = document.getElementById('barras').getContext('2d');
+
+            // Utiliza los datos enviados desde el controlador
+            var clientes = {!! json_encode($cliente->pluck('nombres')) !!};
+            var pedidos = {!! json_encode($pedido->pluck('cantidad')) !!};
+
+            var datos = {
+            labels: clientes,
+            datasets: [{
+                label: 'Cantidad de Pedidos',
+                data: pedidos,
+                backgroundColor: 'rgba(75, 192, 192, 0.2)',
+                borderColor: 'rgba(75, 192, 192, 1)',
+                borderWidth: 1
+            }]
+            };
+
+            var barras = new Chart(ctx, {
+            type: 'bar',
+            data: datos,
+            options: {
+                scales: {
+                y: {
+                    beginAtZero: true
+                }
+                }
+            }
+            });
+        </script>
+        
+        <!-- --------------------------GRAFICO DE LINEAS------------------------------>
+
+        <script>
+            var ctx = document.getElementById('lineas').getContext('2d');
+
+            // Utiliza los datos enviados desde el controlador
+            var pedidos = {!! json_encode($pedido->pluck('descripcion')) !!};
+            var cantidad = {!! json_encode($pedido->pluck('cantidad')) !!};
+
+            var datos = {
+            labels: pedidos,
+            datasets: [{
+                label: 'Nombre de Pedidos',
+                data: cantidad,
+                backgroundColor: 'rgba(75, 192, 192, 0.2)',
+                borderColor: 'rgba(75, 192, 192, 1)',
+                borderWidth: 1
+            }]
+            };
+
+            var lineas = new Chart(ctx, {
+            type: 'line',
+            data: datos,
+            options: {
+                scales: {
+                y: {
+                    beginAtZero: true
+                }
+                }
+            }
+            });
+        </script>
+
+        <!-- --------------------------GRAFICO CIRCULAR------------------------------>
+
+        <script>
+            var ctx = document.getElementById('circular').getContext('2d');
+
+            // Utiliza los datos enviados desde el controlador
+            var pedidosPagados = {!! json_encode($pedido->where('estado', '2')->count()) !!};
+            var pedidosNoPagados = {!! json_encode($pedido->where('estado', '1')->count()) !!};
+
+            var datos = {
+            labels: ['Pagados', ['No Pagados']],
+            datasets: [{
+                label: 'Estado de pedidos',
+                data: [pedidosPagados, pedidosNoPagados],
+                backgroundColor: ['rgba(75, 192, 192, 0.2)','rgba(245, 59, 19, 0.2)'],
+                borderColor: 'rgba(75, 192, 192, 1)',
+                borderWidth: 1
+            }]
+            };
+
+            var circular = new Chart(ctx, {
+            type: 'pie',
+            data: datos,
+            options: {
+                scales: {
+                y: {
+                    beginAtZero: true
+                }
+                }
+            }
+            });
+        </script>
+
     </body>
     </html>
 
