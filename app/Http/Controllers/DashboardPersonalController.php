46e04b17690b100c71783305aa9a01d485431138
KarenPerezVargas <g273300820@unitru.edu.pe>
2023-11-15 23:42:39 -0500

    otra vez dashboard

--- app/Http/Controllers/DashboardPersonalController.php
+++ app/Http/Controllers/DashboardPersonalController.php
@@ -4,19 +4,21 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\Producto;
+use App\Models\Transporte;
+use App\Models\Proveedor;
 
 class DashboardPersonalController extends Controller
 {
     public function graficos(Request $request)
     {
-        $producto = Producto::all();
-        $capacitacion = Capacitacion::all();
-        $empleadoCapacitacion = EmpleadoCapacitacion::all();
+        $producto = Producto::all( );
+        $proveedor = Proveedor::all();
+        $transporte = Transporte::all();
 
-        $capacitacionesPendientes = $capacitacion->where('estadoCapacitacion', 'pendiente')->count();
-        $capacitacionesEnCurso = $capacitacion->where('estadoCapacitacion', 'en curso')->count();
-        $capacitacionesFinalizadas = $capacitacion->where('estadoCapacitacion', 'finalizada')->count();
+        //$capacitacionesPendientes = $capacitacion->where('estadoCapacitacion', 'pendiente')->count();
+        //$capacitacionesEnCurso = $capacitacion->where('estadoCapacitacion', 'en curso')->count();
+        //$capacitacionesFinalizadas = $capacitacion->where('estadoCapacitacion', 'finalizada')->count();
 
-        return view('personalalmacen.graficos', compact('empleado', 'capacitacion', 'empleadoCapacitacion', 'capacitacionesPendientes', 'capacitacionesEnCurso', 'capacitacionesFinalizadas'));
+        return view('personalalmacen.graficos', compact('producto', 'proveedor', 'transporte'));
     }
 }
